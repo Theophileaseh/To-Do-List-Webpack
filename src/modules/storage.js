fad091daa4210b2ccee63ff438f646d38fa2346c
Theophileaseh <theophilenwachan@gmail.com>
2022-03-26 19:17:14 +0100

    Add storage and functionality

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,20 +1,25 @@
 const listsList = document.querySelector('.today-lists');
+const moveDelete = document.querySelector('.single-list-action-button');
+const listValue = document.querySelector('.single-list-input');
 
-let listArray = [];
+export let listArray = [];
 
-const addList = () => {
+export const addList = () => {
+
   let listCode = '';
   listArray.forEach((element, index) => {
+
     const { list } = element;
+    
     if (index % 2 === 0) {
       listCode += `
-        <div class="single-list div-style">
+        <div class="single-list div-style" id="${index}">
           <form class="single-list-form">
             <input type="checkbox" class="checkbox">
-            <input type="text" id="single-list-item" class="single-list-input main-inputs" value="${list}">
+            <input type="text" class="single-list-input main-inputs" value="${list}">
           </form>
           <div class="single-list-action-button">
-          <button class = "move-btn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+            <button class = "delete-btn" onclick="removeList(\'${list}\')"><i class="fa-solid fa-trash-can"></i></button>
           </div>
         </div>
             `;
@@ -23,23 +28,27 @@ const addList = () => {
       <div class="single-list div-style">
       <form class="single-list-form">
         <input type="checkbox" class="checkbox">
-        <input type="text" id="single-list-items" class="single-list-input main-inputs" value="${list}">
+        <input type="text" class="single-list-input main-inputs" value="${list}">
       </form>
       <div class="single-list-action-button">
-      <button class = "move-btn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+        <button class = "delete-btn" onclick="removeList(\'${list}\')"><i class="fa-solid fa-trash-can"></i></button>
       </div>
     </div>
             `;
     }
+    console.log(index);
   });
   listsList.innerHTML = listCode;
   localStorage.setItem('listData', JSON.stringify(listArray));
+  
 };
 
-window.removeBook = (list) => {
+window.removeList = (list) => {
   listArray = listArray.filter((elem) => elem.list !== list);
   addList();
+  // console.log('removed');
 };
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const lists = JSON.parse(localStorage.getItem('listData'));
@@ -57,3 +66,85 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   addList();
 });
+
+
+// add ids
+
+
+
+// input field of list clicked event
+
+/*
+listsList.addEventListener('focusin', (e) => {
+  if(e.target.className.includes('single-list-input')){
+    const parent = e.target.parentNode.parentNode;
+    parent.querySelector('.single-list-action-button').innerHTML = '<button class = "delete-btn" onclick="removeList(\'${list}\')"><i class="fa-solid fa-trash-can"></i></button>';
+  }
+})
+listsList.addEventListener('focusout', (e) => {
+  if(e.target.className.includes('single-list-input')){
+    const parent = e.target.parentNode.parentNode;
+    parent.querySelector('.single-list-action-button').innerHTML = '<button class = "move-btn"><i class="fa-solid fa-ellipsis-vertical"></i></button>';
+
+    const inputValue = e.target.value
+    const {id} = parent.id;
+
+    listArray.forEach(list => {
+      if(id === list.index){
+        list.description = inputValue
+      }
+    })
+
+    localStorage.setItem('listData', JSON.stringify(listArray));
+    
+  }
+})
+*/
+
+
+// drag element
+
+/*
+const dragElement = document.querySelector('.single-list');
+
+dragElement.draggable = 'true';
+
+
+
+ list2.addEventListener("keyup", (event) => {
+  moveDelete.innerHTML = '<button class = "delete-btn" onclick=\'removeList("${list}")\'><i class="fa-solid fa-trash-can"></i></button>'
+});
+
+list2.removeEventListener("click", (event) => {
+  moveDelete.innerHTML = '<button class = "move-btn"><i class="fa-solid fa-ellipsis-vertical"></i></button>'
+});*/
+
+
+// checkbox event
+
+const checkbox = document.querySelector('.checkbox');
+
+listsList.addEventListener("change", function(e) {
+  if(e.target.className.includes('checkbox')){
+    const { checked } = e.target;
+    const text = e.target.parentNode.querySelector('.single-list-input');
+
+    if (checked) {
+      text.classList.add('line-through');
+      list = isCompleted;
+    } else {
+      text.classList.remove('line-through');
+    }
+
+  }
+});
+
+let isCompleted;
+
+// Remove completed list
+
+window.removeIsCompleted = (list) => {
+  listArray = listArray.filter((elem) => list.isCompleted);
+  addList();
+};
+
